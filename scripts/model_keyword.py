ef64b859c9dace617cd1e580398fea45ba5a361e
ChunKoo Park <mix1009@gmail.com>
2023-01-16 23:16:45 +0900

    add UI for setting keyword for current model.

--- scripts/model_keyword.py
+++ scripts/model_keyword.py
@@ -42,6 +42,44 @@ class Script(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
+
+
+        def add_custom(txt):
+            txt = txt.strip()
+            model_ckpt = os.path.basename(shared.sd_model.sd_checkpoint_info.filename)
+            model_hash = get_old_model_hash(shared.sd_model.sd_checkpoint_info.filename)
+            if len(txt) == 0:
+                return f"Enter keyword(trigger word) or keywords separated by |\n\nmodel={model_ckpt}\nmodel_hash={model_hash}"
+            insert_line = f'{model_hash}, {txt}, {model_ckpt}'
+            global scripts_dir
+
+            user_file = f'{scripts_dir}/model-keyword-user.txt'
+            user_backup_file = f'{scripts_dir}/model-keyword-user-backup.txt'
+            lines = []
+
+            with open(user_file, newline='') as csvfile:
+                csvreader = csv.reader(csvfile)
+                for row in csvreader:
+                    try:
+                        mhash = row[0]
+                        if mhash.startswith('#'):
+                            lines.append(','.join(row))
+                            continue
+                        # kw = row[1]
+                        ckptname = None if len(row)<=2 else row[2].strip(' ')
+                        if mhash==model_hash and ckptname==model_ckpt:
+                            continue
+                        lines.append(','.join(row))
+                    except:
+                        pass
+            lines.append(insert_line)
+            csvtxt = '\n'.join(lines) + '\n'
+            import shutil
+            shutil.copy(user_file, user_backup_file)
+            open(user_file, 'w').write(csvtxt)
+
+            return 'added: ' + insert_line
+
         with gr.Group():
             with gr.Accordion('Model Keyword', open=False):
                 is_enabled = gr.Checkbox(label='Model Keyword Enabled', value=True)
@@ -54,6 +92,15 @@ class Script(scripts.Script):
                 multiple_keywords = gr.Dropdown(choices=["keyword1, keyword2", "random", "iterate", "keyword1", "keyword2"],
                                  value='keyword1, keyword2',
                                  label='Multiple keywords:')
+
+                with gr.Accordion('Add Custom Mappings', open=False):
+                    info = gr.HTML("<p style=\"margin-bottom:0.75em\">Add custom keyword(trigger word) mapping for current model. Custom mappings are saved to extensions/model-keyword/model-keyword-user.txt</p>")
+                    text_input = gr.Textbox(placeholder="Keyword or keywords separated by |", label="Keyword(trigger word)")
+                    add_custom_mappings = gr.Button(value='Set Keyword for Model')
+                    text_output = gr.Textbox(interactive=False, label='result')
+
+                    add_custom_mappings.click(add_custom, inputs=text_input, outputs=text_output)
+
 
         return [is_enabled, info, keyword_placement, multiple_keywords]
 
